42c8238a9be8c609a7dbb3d2b9f100f71e0b65f8
SotaSuzuki <3.3sota.suzuki@gmail.com>
2023-02-20 17:18:29 +0900

    add YourProfile comp

---
+++ src/components/YourProfile.tsx
@@ -0,0 +1,93 @@
+import {
+  useReducer,
+  Reducer,
+  useCallback,
+  useState,
+  default as React,
+} from "react";
+
+import styles from "./YourProfile.module.css";
+
+interface State {
+  name: string;
+  age: number;
+}
+
+type Action =
+  | { type: "change_name"; payload: { name: string } }
+  | { type: "increment_age" };
+
+const reducer: Reducer<State, Action> = (state, action) => {
+  switch (action.type) {
+    case "change_name": {
+      return {
+        name: action.payload.name,
+        age: state.age,
+      };
+    }
+    case "increment_age": {
+      return {
+        name: state.name,
+        age: state.age + 1,
+      };
+    }
+  }
+};
+
+const YourProfile = () => {
+  const [state, dispatch] = useReducer(reducer, { name: "YOU", age: 31 });
+  const [newName, setNewName] = useState(() => state.name);
+
+  const handleChangeNewName = useCallback(
+    (evt: React.ChangeEvent<HTMLInputElement>) => {
+      const newName = evt.target.value.trim();
+      setNewName(newName);
+    },
+    [],
+  );
+
+  const handleSubmitNewName = useCallback(
+    (evt: React.MouseEvent<HTMLButtonElement>) => {
+      evt.preventDefault();
+      dispatch({ type: "change_name", payload: { name: newName } });
+      setNewName("");
+    },
+    [newName],
+  );
+
+  const handleClickGetOlder = useCallback(
+    (evt: React.MouseEvent<HTMLButtonElement>) => {
+      evt.preventDefault();
+      dispatch({ type: "increment_age" });
+    },
+    [],
+  );
+
+  return (
+    <div className={styles.wrapper}>
+      <h2>Your Profile</h2>
+      <div>
+        <div>Name: {state.name}</div>
+        <div>Age: {state.age}</div>
+      </div>
+      <div className={styles["form-section"]}>
+        <div>
+          New Name:{" "}
+          <input
+            type="text"
+            value={newName}
+            placeholder="Your new name here"
+            onChange={handleChangeNewName}
+          />
+          <button onClick={handleSubmitNewName}>Submit</button>
+        </div>
+        <div>
+          Increment age:{" "}
+          <button onClick={handleClickGetOlder}>Let&#39;s get older</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default YourProfile;
