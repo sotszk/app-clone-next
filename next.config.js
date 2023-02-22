8c70796c305c1b3094075e6cf102119eb027e9a6
SotaSuzuki <3.3sota.suzuki@gmail.com>
2023-02-22 21:24:34 +0900

    set react strict mode true

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const withVanillaExtract = createVanillaExtractPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   experimental: {
     appDir: false,
   },
