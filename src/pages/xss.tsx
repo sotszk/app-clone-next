6979c5bac97b67904376b16b4312ba474207f9a1
SotaSuzuki <3.3sota.suzuki@gmail.com>
2023-02-25 19:49:42 +0900

    add XSS Testing page

---
+++ src/pages/xss.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import LayoutDefault from "@/layouts/LayoutDefault";
+
+const Xss = () => {
+  const [html, setHtml] = useState("<script>alert('危険');</script>");
+
+  const handleChange = (evt: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setHtml(evt.target.value);
+  };
+
+  return (
+    <LayoutDefault>
+      <h1>XSS TESTING</h1>
+
+      <div>
+        <h2>INPUT</h2>
+        <textarea value={html} onChange={handleChange}></textarea>
+      </div>
+
+      <div>
+        <h2>SAFETY PREVIEW</h2>
+        <div>{html}</div>
+      </div>
+
+      <div>
+        <h2>DANGEROUS PREVIEW</h2>
+        <div dangerouslySetInnerHTML={{ __html: html }}></div>
+      </div>
+    </LayoutDefault>
+  );
+};
+
+export default Xss;
